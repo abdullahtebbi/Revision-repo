ca2e75cd2816beb0fe266291c975f573db3f009f
Abdullah Tayyab <abdullahtebbi@gmail.com>
2025-11-10 23:46:21 +0500

    update

--- Javascript/datatypes/script.js
+++ Javascript/datatypes/script.js
@@ -64,18 +64,124 @@
 // console.log(myName);
 // console.log(typeof myName);
 
+
+
 // ---> convert string to num
 
-// var myFavNum = "3";
-// console.log(myFavNum);
-// console.log(typeof +myFavNum);  // method one
-// console.log(typeof Number(myFavNum));  // method one
+///////
+
+// method one 
+// var numStr = "4";
+// var num = Number(numStr);
+// console.log(num);
+// console.log(typeof num);
+
+// in log
+// console.log(Number(numStr))
+// console.log(typeof Number(numStr));
+
+/////// 
+
+///////
+
+// method two 
+// var numStr = "3";
+// var num = +numStr;
+// console.log(num);
+// console.log(typeof num)
+
+// in log
+// console.log(+numStr);
+// console.log(typeof +numStr);
+
+///////
+
+///////
+
+// method three 
+// var numStr = "3";
+// var num = parseInt(numStr);
+// console.log(num);
+// console.log(typeof num)
+
+// in log
+// console.log(parseInt(numStr));
+// console.log(typeof parseInt(numStr));
+
+//////
+
+//////
+
+// method four
+// var numStr = "3";
+// var num = parseFloat(numStr);
+// console.log(num);
+// console.log(typeof num)
+
+// in log 
+// console.log(parseFloat(numStr));
+// console.log(typeof parseFloat(numStr));
+
+///////
+
+
+
 
 // ---> convert num to string
 
-var str = 3;
-console.log(str);
-console.log(typeof str + "");
+//////
+
+//  method one
+// var str = 3;
+// var numToStr = String(str);
+// console.log(numToStr);
+// console.log(typeof numToStr);
+
+// in log
+// console.log(String(str));
+// console.log(typeof String(str));
+
+///////
+
+///////
+
+//  method two
+// var str = 4;
+// var numTwoStr =  str.toString();
+// console.log(numTwoStr);
+// console.log(typeof numTwoStr);
+
+// in log
+// console.log(str.toString());
+// console.log(typeof str.toString());
+
+///////
+
+///////
+
+// method three
+// var str = 4;
+// var numToStr = `${str}`;
+// console.log(numToStr);
+// console.log(typeof numToStr);
+
+// in log
+// console.log(`${str}`);
+// console.log(typeof `${str}`);
+
+///////
+
+///////
+
+// method four
+// var str = 2;
+// var numToStr = str + "";
+// console.log(numToStr);
+// console.log(typeof numToStr)
+
+// in log 
+// console.log(str + "");
+// console.log(typeof (str + ""));
 
 
 
---
+++ Javascript/operators/script.js
@@ -0,0 +1,161 @@
+///////////////////////////// types of opertators
+
+//////
+
+// ----> assignment operators (=) 
+// (assignment oprators in programing are symbols used to assign values to variables. They take the value on the right side of the operator and assign it to the variable on the left side.)
+
+// code
+// var myFavNum = 3;
+
+// note (= is a assignment operator)
+
+//////
+
+//////
+
+// ---> Arithmetic operators 
+// (Arithmetic operators in programing perform basic mathematical operations on variables or values. They include addition, substraction, multiplication, divisions and modulus.)
+
+// code
+// --> addition (+) : add two values or variables 
+
+// var x = 4;
+// var y = 6;
+// var sum = x + y;
+// console.log(sum);
+
+
+// --> substraction (-) : subsracts the right operand to the left oparend
+
+// var x = 8;
+// var y = 3;
+// var final = x - y;
+// console.log(final);
+
+
+// --> multiplication (*) : multiplies two variables or values
+
+// var x = 8;
+// var y = 2;
+// var final = x * y;
+// console.log(final);
+
+
+// --> division (/) : devide the left operand by the right operand
+
+// var x = 8;
+// var y = 2;
+// var final = x / y;
+// console.log(final);
+
+
+// --> modulus (%) : returns the reminder when the left operand is devided by the right operend.
+
+// var x = 9;
+// var y = 2;
+// var final = x % y;
+// console.log(final);
+
+
+// --> extra consepts
+
+// -> toFixed() function
+// var x = 0.1;
+// var y = 0.2;
+// console.log(x + y);
+// output -> 0.30000000000000004
+
+// solution 
+// console.log((x + y).toFixed(1));
+
+//////
+
+//////
+
+// ---> string operator 
+// There are a few ways to concatenate strings in javascript. The most common is to use the + operator. For example, to concatenate the strings "Hello" and "World", you would use the following code:
+
+// var str1 = "Hello ";
+// var str2 = "World";
+// var str3 = str1 + str2;
+// console.log(str3);
+
+//////
+
+//////
+
+// ---> comparison operator
+// conparison operator in javascript are used to compare values and return a boolean result (true or false).
+
+// --> Equal (==) Checks if two values are equal, performing type coercion if necessary.
+//  console.log(5 == 10); // false
+//  console.log(5 == 5); // true
+
+ 
+// --> strick equal (===)
+// check if two values are equal without performing type corection.
+// console.log(5 === "5"); // false
+// console.log("5" === "5"); // true
+// console.log("10" === "5"); // false
+// console.log(5 === 5); // false
+
+
+// --> not equal to (!=)
+// check if two values are not equal, performing type coercion if necessary. 
+// console.log(5 != 4); // true
+// console.log(5 != 5); // false
+// console.log(5 != "4"); // true
+
+
+// --> grater than (>)
+// check if the value on the left is grater then the value on the right.
+// console.log(5 > 2); // true
+// console.log(5 > 10); // false
+
+
+// --> less than (<)
+// check if the value on the left is less then the value on the right.
+// console.log(5 < 2); // false
+// console.log(5 < 8); // true
+
+
+// --> grater than or equal to (>=)
+// check if the value on the left is grater than or equal to the value on the right.
+// console.log(5 >= 2); // true
+// console.log(5 >= 5); // true
+// console.log(5 >= 6); // false
+
+
+// --> less than or equal to (<=)
+// check if the value on the left is less than or equal to the value on the right.
+// console.log(4 <= 5); // true
+// console.log(4 <= 4); // true
+// console.log(4 <= 2); // false
+
+
+//////
+
+//////
+
+// ---> logical operator
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
