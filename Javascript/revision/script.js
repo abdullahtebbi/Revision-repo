c5e2e500b21076606bb6a632b5084b7eb8fa4e24
Abdullah Tayyab <abdullahtebbi@gmail.com>
2026-01-04 22:37:42 +0500

    update

---
+++ Javascript/revision/script.js
@@ -0,0 +1,120 @@
+//                  if else statement
+
+// let candidateAge = 18;
+// let isCitizen = true ;
+// let isRegistered = false;
+
+
+// if(candidateAge >= 18){
+//     if(isCitizen === true){
+//       if(isRegistered === true){
+//     console.log("They are eligible to vote");
+//       }else{
+//     console.log("They are not eligibile due to registration status")
+//       }
+//     }else{
+//     console.log("Not eligible due to citizenship status")
+//     }
+// }else{
+//     console.log("They are not eligivle to vote");
+// }
+
+
+
+
+
+
+
+
+
+
+//                   switch statement 
+
+// let day = "Saturday";
+
+// switch(day){
+//     case "Monday":
+//         console.log("Learning at home");
+//         break;
+
+//     case "Tuesday":
+//         console.log("Learning at home");
+//         break;
+        
+//     case "Wednesday":
+//         console.log("Learning at home");
+//         break;
+        
+//     case "Thursday":
+//         console.log("Learning at home");
+//         break;
+        
+//     case "Friday":
+//         console.log("Complete pending works of home");
+//         break;
+
+//     case "Saturday":
+//         console.log("Going to university");
+//         break;
+        
+//     case "Sunday":
+//         console.log("Going to university")    
+//         break;
+ 
+//     default :
+//         console.log("No condition match")   
+// }
+
+
+
+//                       loops
+
+
+// --> while loop
+
+// let num = 1;
+
+// while(num <= 10){
+//     console.log(num);
+//     num++;
+// };
+
+
+// --> do while loop
+
+// let num = 16;
+
+// do{
+//     console.log(num);
+//     num++;
+// }while(num <= 10);
+
+
+// let num = 16; // conditon is false but run code atleast one time
+
+// do{
+//     console.log(num);
+//     num++;
+// }while(num <= 10);
+
+
+// --> for loop 
+
+for(let num = 1; num <= 10; num++){
+    console.log(num);
+}
+
+
+for(let num = 1; num <= 10; num++){
+    console.log(`10 * ${num} = ${num * 10}`);
+}
+
+
+
+
+
+
+
+
+
+
