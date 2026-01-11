0d96caf97bb18bc10f21c9c4df203be804580a3f
Abdullah Tayyab <abdullahtebbi@gmail.com>
2026-01-11 23:53:07 +0500

    update

--- Javascript/revision/script.js
+++ Javascript/revision/script.js
@@ -225,14 +225,14 @@
 // isReverse("hello world")
 
 
-const isReverse = (str) => {
-    let reverse = "";
-    for(let char = str.length -1; char >= 0; char--) {
-        reverse = reverse + str[char];
-    }
-    return reverse;
-};
-console.log(isReverse("hello world"))
+// const isReverse = (str) => {
+//     let reverse = "";
+//     for(let char = str.length -1; char >= 0; char--) {
+//         reverse = reverse + str[char];
+//     }
+//     return reverse;
+// };
+// console.log(isReverse("hello world"))
 
 
 
@@ -240,6 +240,152 @@ console.log(isReverse("hello world"))
 
 
 //                       Array
+
+// ---> creat array
+
+// --> Using array constructor
+
+// let fruits = new Array ('apple', 'orange', 'banana');
+// console.log(fruits);
+
+
+// --> Using array literal
+
+// let fruits = ["apple", "orange", "banana"];
+// console.log(fruits);
+
+
+// --> we can also creat an empty array
+
+// let arr = [];
+// console.log(typeof arr); // object
+
+
+
+
+
+// ---> Modifying elements
+
+// let fruits = ["apple", "banana"];
+// console.log(fruits);
+// fruits[2] = "grapes"; // add value in the end array
+// // fruits[1] = "grapes"; // replase the value of the index
+// console.log(fruits);
+
+
+
+
+// --- > Array Traversal / Iterating over array
+
+// --> for of loop , also known as iterable 
+// let fruits = ["Apple", "Orange", "Mango", "Grapes", "Banana"]
+
+// for (let fruit of fruits){
+//     console.log(fruit);
+// };
+
+
+// --> for in loop , iterate over the properties (including indices) of an object 
+// let fruits = ["Apple", "Orange", "Mango", "Grapes", "Banana"]
+
+// for (let fruit in fruits){
+//     console.log(fruit);
+// };
+
+
+
+
+
+// --> forEach method
+
+// let fruits = ["Apple", "Orange", "Mango", "Grapes", "Banana"]
+// fruits.forEach((currEle,index,arr)=>{
+//      console.log(`${currEle} ${index}`);
+
+//     //  console.log(arr);
+// });
+
+// -> return (No)
+// let fruits = ["Apple", "Orange", "Mango", "Grapes", "Banana"]
+// const myForEachArr = fruits.forEach((currVal , index , arr) => {
+//     return `${currVal} ${index}`;
+// }); 
+// console.log(myForEachArr);
+
+// Note : Use the forEach method to performs an action on each element
+
+
+// --> map method
+
+// let fruits = ["Apple", "Orange", "Mango", "Grapes", "Banana"]
+// fruits.map((currEle,index,arr)=>{
+//      console.log(`${currEle} ${index}`);
+
+//     //  console.log(arr);
+// });
+
+
+// -> return (Yes)
+// let fruits = ["Apple", "Orange", "Mango", "Grapes", "Banana"]
+// const myMapArr = fruits.map((currVal , index , arr) => {
+//     return `${currVal} ${index}`;
+// }); 
+// console.log(myMapArr);
+
+// Note : Use the .map method creates a new array with transformed alements
+
+
+
+
+// ---> Aray methods
+
+// unshift()  // add the element to the start of the array
+// let fruits = ["Apple", "Orange", "Mango"]
+// fruits.unshift("banana");
+// console.log(fruits);
+
+// --> push() add element to the end of array
+// let fruits = ["Apple", "Orange", "Mango"]
+// fruits.push("Grapes")
+// console.log(fruits);
+
+
+// shift() delete first element of the array
+// let fruits = ["Apple", "Orange", "Mango"]
+// fruits.shift();
+// console.log(fruits);
+
+
+// pop() delete last element of the array
+// let fruits = ["Apple", "Orange", "Mango"]
+// fruits.pop();
+// console.log(fruits);
+
+
+// splice(start-index,delete-count,"add new element");
+// let fruits = ["Apple", "Orange", "Mango"]
+// fruits.splice(1,1,"Banana");
+// console.log(fruits);
+
+
+// indexOf(searching-value,from-index search start to upword);
+// let fruits = ["Apple", "Orange", "Mango","Grapes", "Banana"]
+// console.log(fruits.indexOf("Grapes",1));
+
+
+// lastIndexOf(searching-value, from index search start to backword)
+// let numbers = [1,2,3,6,5,6,7,8,9];
+// console.log(numbers.lastIndexOf(6,5));
+
+
+// includex(searching value) // searching the value and return true or false
+// let fruits = ["Apple", "Orange", "Mango"]
+// console.log(fruits.includes("Mango"));
+
+
+
+
+
 
 
 
